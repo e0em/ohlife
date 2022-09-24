437e716eb4a164845477155446f54363f8a79b0f
Marty@W11.WSLg2 <marty@freedom-tek.com>
2022-09-24 18:55:38 +0800

    add: debug for text option

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-cat ./tmp.sample/sample.eml| /home/marty/miniconda3/envs/ohlife/bin/python sendmail_pipe.py
+cat ./tmp.sample/sample.eml| /home/marty/miniconda3/envs/ohlife/bin/python sendmail_pipe.py text
